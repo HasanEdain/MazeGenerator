a7fbd82e07159e3d1240d9cbb10a51f6b5fe7095
Hasan Edain <hasane@npcunlimited.com>
2024-09-30 19:05:54 -0700

    Get save working.

--- MazeGenerator/GenerateView.swift
+++ MazeGenerator/GenerateView.swift
@@ -16,6 +16,10 @@ struct GenerateView: View {
     @State var offset: Int = 30
     @State var tileSize: Int = 25
 
+    @State var filename: String = "puzzle"
+    @State var saveCountString: String = "0"
+
+
     var body: some View {
         VStack {
             HStack {
@@ -25,20 +29,31 @@ struct GenerateView: View {
             Form {
                 TextField(text: $mazeWidthString) {
                     Label("Width", systemImage: "arrow.left.and.line.vertical.and.arrow.right")
-                }.padding()
+                }
                 TextField(text: $mazeHeightString) {
                     Label("Height", systemImage: "arrow.up.and.line.horizontal.and.arrow.down")
-                }.padding()
+                }
                 TextField(text: $offsetString) {
                     Label("Offset", systemImage: "arrow.right")
-                }.padding()
+                }
                 TextField(text: $tileSizeString) {
                     Label("Tile Size", systemImage: "arrow.left.and.right.square")
-                }.padding()
+                }
                 Button("Generate") {
                     gen()
+                }.padding(.bottom)
+                TextField(text: $filename) {
+                    Label("Filename", systemImage: "richtext.page.fill")
                 }
-            }.frame(width: 300)
+                TextField(text: $saveCountString) {
+                    Label("Save Count", systemImage: "richtext.page.fill")
+                }
+                Button("Save") {
+                    save()
+                }.padding(.bottom)
+            }
+            .padding()
+            .frame(width: 300)
         }
     }
 
@@ -68,6 +83,60 @@ struct GenerateView: View {
         let aMaze = Maze(height: mazeHeight, width: mazeWidth)
         
         self.maze = aMaze
+    }
+
+    @MainActor func save() {
+        let rendererSolved = ImageRenderer(content: MazeView(maze: maze, isSolved: true, offset: 0, tileSize: self.tileSize))
+        let redererPuzzle = ImageRenderer(content: MazeView(maze: maze, isSolved: false, offset: 0, tileSize: self.tileSize))
+
+        guard let saveCount = Int(self.saveCountString) else {
+            return
+        }
+
+        let homeURL = FileManager.default.homeDirectoryForCurrentUser
+        let solvedUrl = homeURL.appending(path: "\(filename)_\(saveCount)_solved.pdf")
+        print("\(solvedUrl.absoluteString)")
+        let unsolvedUrl = homeURL.appending(path: "\(filename)_\(saveCount)_puzzle.pdf")
+        guard let width = Float(mazeWidthString) else  {
+            return
+        }
+        guard let height = Float(mazeHeightString) else {
+            return
+        }
+
+        rendererSolved.render { size, renderInContext in
+            var box = CGRect(
+                origin: .zero,
+                size: .init(width: CGFloat(width)*CGFloat(tileSize), height: CGFloat(height)*CGFloat(tileSize))
+            )
+
+            guard let context = CGContext(solvedUrl as CFURL, mediaBox: &box, nil) else {
+                return
+            }
+
+            context.beginPDFPage(nil)
+            renderInContext(context)
+            context.endPage()
+            context.closePDF()
+        }
+
+        redererPuzzle.render { size, renderInContext in
+            var box = CGRect(
+                origin: .zero,
+                size: .init(width: CGFloat(width)*CGFloat(tileSize), height: CGFloat(height)*CGFloat(tileSize))
+            )
+
+            guard let context = CGContext(unsolvedUrl as CFURL, mediaBox: &box, nil) else {
+                return
+            }
+
+            context.beginPDFPage(nil)
+            renderInContext(context)
+            context.endPage()
+            context.closePDF()
+        }
+
+        self.saveCountString = String(saveCount + 1)
     }
 }
 
